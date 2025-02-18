021279af47994974b1223567d022180c827c973d
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-02-18 17:01:25 +0200

    add-user-controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
