68c764b76cc94390229976add1a82f18e7a0ea7e
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-02-18 16:34:47 +0200

    add-getMe

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
