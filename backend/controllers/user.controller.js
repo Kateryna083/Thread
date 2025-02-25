028ae2a954dc750cf4777060c56f9ddaaa6b56a6
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-02-25 14:57:30 +0200

    added-notification

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,6 +10,8 @@ export const getUserProfile = async (req, res) => {
   try {
     const user = await User.findOne({ username }).select("-password");
     if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json(user);
   } catch (error) {
     console.log("Error in getUserProfile: ", error.message);
     res.status(500).json({ error: error.message });
