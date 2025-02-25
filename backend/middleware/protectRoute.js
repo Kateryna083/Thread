fafd152464fab91bf26dcb1a3e502343b045835c
Kateryna083 <159190252+Kateryna083@users.noreply.github.com>
2025-02-25 15:06:08 +0200

    back-done

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,5 +1,6 @@
-import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
